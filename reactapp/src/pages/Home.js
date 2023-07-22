4fe32a60d65380f8a572d6657173abf171d6b536
Felix <ezamabusiness@gmail.com>
2023-07-22 17:08:41 -0600

    commit

--- reactapp/src/pages/Home.js
+++ reactapp/src/pages/Home.js
@@ -1,19 +1,21 @@
+import * as React from "react";
+import { Box, Paper } from "@mui/material";
+import profile from "../data/profile";
+
+const DataBlock = (props) => {
+  const boxStyle = {
+    width: 400,
+    height: 400,
+    marginRight: 100,
+    top: 100,
+    marginTop: -60, // Adjust the marginTop value as needed
+    backgroundColor: "#FFD700",
+    color: "var(--tertiary-color)",
+    display: "inline-block", // Optional: Use this property to adjust the positioning in relation to other elements
+  };
+
+  return <Paper elevation={3} sx={boxStyle}></Paper>;
+};
 export default function Home() {
-  return (
-    <>
-      <html>
-        <head>
-          <script></script>
-        </head>
-        <body>
-          <canvas
-            id="lamp-anim"
-            class="lamp-anim"
-            width="1034"
-            height="613"
-          ></canvas>
-        </body>
-      </html>
-    </>
-  );
+  return <DataBlock></DataBlock>;
 }
