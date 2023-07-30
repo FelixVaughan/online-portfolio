5982ecc9207ab72c5970fdea65edd95680dd50a0
Felix <ezamabusiness@gmail.com>
2023-07-30 00:37:48 -0600

    Removed projects component and related files and made UI changes to home page

--- reactapp/src/pages/home/Home.js
+++ reactapp/src/pages/home/Home.js
@@ -1,15 +1,26 @@
-import Profile from "../../components/profile/Profile";
-import profileData from "../../data/profile";
-import profilePic from "../../assets/profile.jpg";
+import image from "../../assets/profile.jpg";
 import "./home.css";
 export default function Home() {
   return (
     <div classname="container">
       <div classname="header-section">
-        <Profile bio={profileData.bio} image={profilePic} />;
+        <div className="profile-container">
+          <div className="profile-picture">
+            <img src={image} width={3024} height={4024} alt="profile.jpg" />
+          </div>
+          <div className="profile-header">
+            <p>
+              Greetings, I'm Felix;
+              <br />
+              An Aspiring young professional embarking on an exciting journey
+              driven by a passion for innovation and a vision to create
+              groundbreaking advancements that shape the future.
+            </p>
+          </div>
+        </div>
       </div>
       <hr />
-      <div className="description-section">{profileData.info}</div>
+      <div className="description-section"></div>
     </div>
   );
 }
