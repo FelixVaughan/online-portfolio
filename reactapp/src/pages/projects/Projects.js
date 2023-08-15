81d0ac46bef16593896f5d6dab4712127c950aec
Felix <ezamabusiness@gmail.com>
2023-08-14 23:48:45 -0600

    additional stylings to projects page and initial scrollbar stylings

--- reactapp/src/pages/projects/Projects.js
+++ reactapp/src/pages/projects/Projects.js
@@ -1,8 +1,26 @@
 import CIcon from "@coreui/icons-react";
 import { Card, CardContent, Grid, Typography } from "@mui/material/";
 import { FaGithubAlt } from "react-icons/fa";
+import "./projects.css";
 
 function Project({ header, content, path, stack, reverseOrder = false }) {
+  const styles = {
+    card: {
+      maxWidth: 345,
+      marginLeft: reverseOrder ? "0" : "auto",
+      marginBlock: 2.5,
+      backgroundColor: "var(--card)",
+      height: 200,
+      minWidth: 500,
+    },
+
+    typography: {
+      color: "var(--primary)",
+      fontSize: 15,
+      fontWeight: "bold",
+    },
+  };
+
   const Img = (
     <Grid item xs={12} sm={6} className="project-img" sx={{ color: "white" }}>
       IMG
@@ -17,10 +35,10 @@ function Project({ header, content, path, stack, reverseOrder = false }) {
       className="project-desc"
       sx={{ color: "white", textAlign: reverseOrder ? "left" : "right" }}
     >
-      <h1>{header}</h1>
-      <Card sx={{ maxWidth: 345, marginLeft: reverseOrder ? "0" : "auto" }}>
+      <h1 className="project-title">{header}</h1>
+      <Card sx={styles.card}>
         <CardContent>
-          <Typography variant="body2" color="text.secondary">
+          <Typography variant="body2" sx={styles.typography}>
             {content}
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
@@ -36,7 +54,7 @@ function Project({ header, content, path, stack, reverseOrder = false }) {
             </>
           );
         })}
-      <FaGithubAlt />
+      <FaGithubAlt color="var(--secondary)" />
     </Grid>
   );
 
@@ -51,7 +69,13 @@ function Project({ header, content, path, stack, reverseOrder = false }) {
 export default function Projects() {
   return (
     <>
-      <h1>Things I've Built</h1>
+      <div id="projects-header">
+        <h1 id="projects-header-text">Things I've Built</h1>
+        <hr
+          id="projects-header-divider"
+          style={{ borderColor: "var(--secondary)" }}
+        />
+      </div>
       <Grid
         container
         spacing={10}
