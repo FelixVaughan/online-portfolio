4b6577b47244230dc33f145268e39bd1f2c158e1
Felix <ezamabusiness@gmail.com>
2023-08-23 14:31:54 -0600

    Conditional navbar background after scroll point

--- reactapp/src/components/navbar/Navigation.js
+++ reactapp/src/components/navbar/Navigation.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "./navigation.css";
 
 export default function Navigation() {
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > 100) {
+        // Change 100 to the desired scroll height
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <Navbar className="nav-container" expand="lg">
+    <Navbar
+      className={`nav-container ${showBackground ? "show-bg" : ""}`}
+      expand="lg"
+    >
       <Container>
         <Nav className="me-auto nav-items-container">
           <Nav.Link href="/">Home</Nav.Link>
