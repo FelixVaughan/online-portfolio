0b5782d5fe4054b82d154db9fb75323ff364bb1d
Felix <ezamabusiness@gmail.com>
2023-09-24 00:13:40 -0600

    start script

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+npm run build --prefix reactapp
+cd aws
+sam build && sam deploy --no-confirm-changeset
+bucketname=$(aws cloudformation describe-stacks --stack-name portfolio-infrastructure --query "Stacks[0].Outputs[?OutputKey=='BucketName'].OutputValue" --output text)
+aws s3 sync ../reactapp/build s3://$bucketname
+echo "Complete...!"
+exit
