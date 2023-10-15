c732676b87a15ce2666503dd3faf98546f7b29ae
Felix <ezamabusiness@gmail.com>
2023-10-14 20:10:23 -0600

    done

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -6,5 +6,4 @@ cd aws
 sam build && sam deploy --no-confirm-changeset
 bucketname=$(aws cloudformation describe-stacks --stack-name portfolio-infrastructure --query "Stacks[0].Outputs[?OutputKey=='BucketName'].OutputValue" --output text)
 aws s3 sync ../reactapp/build s3://$bucketname
-echo "Complete...!"
-exit
+echo "Stack deployed, remember to re-sync bucket..."
