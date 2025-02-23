29598ce5fc163945f3004f44614d4f8c414df532
Felix Ezama-Vaughan <ezamabusiness@gmail.com>
2025-02-23 12:50:43 -0700

    Branch: main Date: 2025-02-23 12:50:43 Author: Felix Ezama-Vaughan
    Changes
    2 line(s) changed
    1 file(s) changed
    
    Modified files:
    - buildAndDeploy.sh (+2, -1)

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -6,4 +6,5 @@ cd aws
 sam build && sam deploy --no-confirm-changeset
 bucketname=$(aws cloudformation describe-stacks --stack-name portfolio-infrastructure --query "Stacks[0].Outputs[?OutputKey=='BucketName'].OutputValue" --output text)
 aws s3 sync ../reactapp/build s3://$bucketname
-echo "Stack deployed, remember to re-sync bucket..."
+aws cloudfront create-invalidation  --distribution-id $PORT_CLOUNDFRONT_ID --paths "/*"
+echo "Deployment completed"
